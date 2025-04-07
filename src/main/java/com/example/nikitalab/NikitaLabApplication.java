cc7ee305cf976077865462b6e9bc8abaa8076556
returnaction <obergannv@gmail.com>
2025-04-07 22:43:19 +0300

    Init Project

---
+++ src/main/java/com/example/nikitalab/NikitaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.nikitalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikitaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikitaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nikitalab/NikitaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikitalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikitaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
